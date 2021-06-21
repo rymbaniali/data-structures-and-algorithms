43ddad4160d52c437bd26793c12b01c1bc1b8dd0
rymbaniali <rym.ayasrah@hotmail.com>
2021-06-21 17:16:05 +0300

    modified code challenge 11

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -25,8 +25,6 @@ function transformToLis(obj) {
   }
   return arr;
 }
-
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
 
@@ -36,6 +34,7 @@ Note: You might need to use the same method more than once.
 
 For example, count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]]) returns 4.
 ------------------------------------------------------------------------------------------------ */
+
 
 const count = (target, input) => {
   let count = 0;
@@ -68,7 +67,6 @@ const totalSum = (input) => {
   });
   return count;
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
 
@@ -94,7 +92,6 @@ const divisibleByFiveTwoToThePower = (input) => {
   });
   return arr;
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5 
 
@@ -165,7 +162,6 @@ let findMaleAndFemale = (data) => {
   });
   return string.join(' and ');
 };
-
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6 
 
@@ -187,68 +183,68 @@ let findShortest = (data) => {
 
   };
 
-  /* ------------------------------------------------------------------------------------------------
-  TESTS
-  
-  All the code below will verify that your functions are working to solve the challenges.
-  
-  DO NOT CHANGE any of the below code.
-  
-  Run your tests from the console: jest challenges-10.test.js
-  
-  ------------------------------------------------------------------------------------------------ */
+/* ------------------------------------------------------------------------------------------------
+TESTS
 
-  describe('Testing challenge 1', () => {
-    test('It should return a list of key value pairs inside of li tags', () => {
-      expect(transformToLis({ name: 'bob', age: 32 })[0]).toStrictEqual(`<li>name: bob</li>`);
-      expect(transformToLis({ name: 'bob', age: 32 })[1]).toStrictEqual(`<li>age: 32</li>`);
-      expect(transformToLis({})).toStrictEqual([]);
-    });
+All the code below will verify that your functions are working to solve the challenges.
+
+DO NOT CHANGE any of the below code.
+
+Run your tests from the console: jest challenges-10.test.js
+
+------------------------------------------------------------------------------------------------ */
+
+describe('Testing challenge 1', () => {
+  test('It should return a list of key value pairs inside of li tags', () => {
+    expect(transformToLis({name: 'bob', age: 32})[0]).toStrictEqual(`<li>name: bob</li>`);
+    expect(transformToLis({name: 'bob', age: 32})[1]).toStrictEqual(`<li>age: 32</li>`);
+    expect(transformToLis({})).toStrictEqual([]);
+  });
+});
+
+describe('Testing challenge 2', () => {
+  test('It should return the number of times the input is in the nested arrays', () => {
+    expect(count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(4);
+    expect(count(3, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(2);
+    expect(count(12, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(0);
+  });
+  test('It should work on empty arrays', () => {
+    expect(count(5, [[1, 3, 5, 7, 9], [], [5, 5, 5], [1, 2, 3], []])).toStrictEqual(4);
+    expect(count(5, [])).toStrictEqual(0);
+  });
+});
+
+describe('Testing challenge 3', () => {
+  test('It should add all the numbers in the arrays', () => {
+    const nums = [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]];
+
+    expect(totalSum(nums)).toStrictEqual(66);
+  });
+});
+
+describe('Testing challenge 4', () => {
+  test('It should return numbers divisible by five, then raise two to the power of the resulting numbers', () => {
+    expect(divisibleByFiveTwoToThePower([[10, 20, 5, 4], [5, 6, 7, 9], [1, 10, 3]])).toStrictEqual([[1024, 1048576, 32], [32], [1024]]);
   });
 
-  describe('Testing challenge 2', () => {
-    test('It should return the number of times the input is in the nested arrays', () => {
-      expect(count(5, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(4);
-      expect(count(3, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(2);
-      expect(count(12, [[1, 3, 5, 7, 9], [5, 5, 5], [1, 2, 3]])).toStrictEqual(0);
-    });
-    test('It should work on empty arrays', () => {
-      expect(count(5, [[1, 3, 5, 7, 9], [], [5, 5, 5], [1, 2, 3], []])).toStrictEqual(4);
-      expect(count(5, [])).toStrictEqual(0);
-    });
+  test('It should return an empty array if none of the numbers are divisible by five', () => {
+    expect(divisibleByFiveTwoToThePower([[1, 2, 3], [5, 10, 15]])).toStrictEqual([[], [32, 1024, 32768]]);
   });
 
-  describe('Testing challenge 3', () => {
-    test('It should add all the numbers in the arrays', () => {
-      const nums = [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]];
-
-      expect(totalSum(nums)).toStrictEqual(66);
-    });
+  test('It should return an empty array if the values are not numbers', () => {
+    expect(divisibleByFiveTwoToThePower([['one', 'two', 'five'], ['5', '10', '15'], [5]])).toStrictEqual([[], [], [32]]);
   });
+});
 
-  describe('Testing challenge 4', () => {
-    test('It should return numbers divisible by five, then raise two to the power of the resulting numbers', () => {
-      expect(divisibleByFiveTwoToThePower([[10, 20, 5, 4], [5, 6, 7, 9], [1, 10, 3]])).toStrictEqual([[1024, 1048576, 32], [32], [1024]]);
-    });
-
-    test('It should return an empty array if none of the numbers are divisible by five', () => {
-      expect(divisibleByFiveTwoToThePower([[1, 2, 3], [5, 10, 15]])).toStrictEqual([[], [32, 1024, 32768]]);
-    });
-
-    test('It should return an empty array if the values are not numbers', () => {
-      expect(divisibleByFiveTwoToThePower([['one', 'two', 'five'], ['5', '10', '15'], [5]])).toStrictEqual([[], [], [32]]);
-    });
+describe('Testing challenge 5', () => {
+  test('It should return only characters that are male or female', () => {
+    expect(findMaleAndFemale(starWarsData)).toStrictEqual('Luke Skywalker and Darth Vader and Leia Organa');
+    expect(findMaleAndFemale([{ name: 'person', gender: 'female' }, { gender: 'lol' }, { name: 'persontwo', gender: 'male' }])).toStrictEqual('person and persontwo');
   });
+});
 
-  describe('Testing challenge 5', () => {
-    test('It should return only characters that are male or female', () => {
-      expect(findMaleAndFemale(starWarsData)).toStrictEqual('Luke Skywalker and Darth Vader and Leia Organa');
-      expect(findMaleAndFemale([{ name: 'person', gender: 'female' }, { gender: 'lol' }, { name: 'persontwo', gender: 'male' }])).toStrictEqual('person and persontwo');
-    });
+describe('Testing challenge 6', () => {
+  test('It should return the name of the shortest character', () => {
+    expect(findShortest(starWarsData)).toStrictEqual('R2-D2');
   });
-
-  describe('Testing challenge 6', () => {
-    test('It should return the name of the shortest character', () => {
-      expect(findShortest(starWarsData)).toStrictEqual('R2-D2');
-    });
-  });
+});
